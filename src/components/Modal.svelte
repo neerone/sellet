2e1adc41e72e93da0b403d371c38ee8ae9202c94
Tony Plato <neuroweet@gmail.com>
2019-09-22 10:38:28 +0300

    some transitions, design enhancments.

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -1,6 +1,17 @@
 <script>
+    import { fly, fade } from 'svelte-transitions';
     let header;
     let onClose;
+
+    function wrapperClose() {
+        onClose()
+    }
+
+    function preventClose(event) {
+        event.stopPropagation()
+        event.stopImmediatePropagation()
+        event.preventDefault()
+    }
 
     export { header, onClose }
 </script>
@@ -17,37 +28,43 @@
     	display: flex;
     	justify-content: center;
     	align-items: center;
-        font-family: Helvetica Neue,Helvetica,Arial,sans-serif;
+        font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
     }
     .sellet-modal-inner-wrapper {
         background: white;
-        padding: 10px;
-        min-width: 300px;
+        min-width: 310px;
+        max-width: 450px;
+        width:100%;
         position: relative;
+        border-radius: 5px;
+        box-shadow: 0px 1px 3px #0000000d;
     }
     .sellet-modal-close {
         display: block;
         position: absolute;
         right: 0px;
         top: 0px;
-        color: #a70000f2;
         font-size: 30px;
         font-family: impact;
-        padding: 10px;
+        padding: 14px;
         line-height: 14px;
         cursor: pointer;
+        color: #ff5b5bf2;
     }
     .sellet-modal-header {
-        font-size: 150%;
+        padding: 10px 15px;
+        border-bottom: 1px solid #cccccc;
+        font-size: 120%;
+        line-height: 120%;
     }
     .sellet-modal-content {
-        margin-top: 10px;
+        padding: 10px 15px;
     }
 </style>
 
 
-<div class='sellet-modal-wrapper'>
-    <div class='sellet-modal-inner-wrapper'>
+<div class='sellet-modal-wrapper' on:click={wrapperClose} transition:fade="{{ duration: 300 }}">
+    <div class='sellet-modal-inner-wrapper' on:click={preventClose} transition:fly="{{ y: 200, duration: 300 }}">
         <div class='sellet-modal-close' on:click={onClose}>×</div>
         <div class='sellet-modal-header'>{header}</div>
         <div class='sellet-modal-content'><slot/></div>
